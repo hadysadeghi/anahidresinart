ed6a6d58952486dcb998c0966c64f9bcbf4b63a0
hadysadeghi <71498978+hadysadeghi@users.noreply.github.com>
2025-09-21 13:26:56 +0330

    feat: Implement core MVP with product database, admin panel, and API

--- anahidresinart/urls.py
+++ anahidresinart/urls.py
@@ -14,9 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from products.views import ProductViewSet
+from django.conf import settings
+from django.conf.urls.static import static
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet, basename='product')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
